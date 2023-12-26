39044cea1c076ceedeb69b77c8bab8f48357a76a
Angel Romero <mrangelrcd@gmail.com>
2023-12-26 18:19:06 +0100

    Part 1 exercise 1.3, 1.4, 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,18 +4,19 @@ const Header = ({course}) => {
   )
 }
 
-const Part = ({part, exercise})=> {
+const Part = ({part})=> {
+  const {name, exercises} = part;
   return (
-    <p>{part} {exercise}</p>
+    <p>{name} {exercises}</p>
   )
 }
 
-const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+const Content = ({parts}) => {
   return (
     <>
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </>
   )
 }
@@ -25,19 +26,33 @@ const Total = ({total}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
+  const total = course.parts.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.exercises,
+    0,
+  )
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={total} />
     </div>
   )
 }
